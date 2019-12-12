ec41a0697d93d8e109c3d13ce14b48f8c1a79c7b
Karamchi <karamchi.ghandi@gmail.com>
2019-12-12 13:39:20 -0300

    Update scorebot

--- Scorebot/server.js
+++ Scorebot/server.js
@@ -1,13 +1,15 @@
 const Telegraf = require('telegraf')
+const schedule = require('node-schedule')
+const Matrixes = require('./matrixes')
 var fs = require('fs');
 
 const bot = new Telegraf(process.env.TOKEN)
 
-var messageLikers = {}
 var lastMessage = {}
 var lastMessagePosta = {}
 const ME = 160565993
 const WITTY = 573582514
+const SMAUGS = -226076541
 
 var toAscii = function(str) {
         return str.toLowerCase()
@@ -24,12 +26,6 @@ var toAscii = function(str) {
 bot.start((ctx) => ctx.reply('Welcome!'))
 
 bot.on('sticker', (ctx) => {
-    /*em = ctx.message.sticker.emoji
-    if ("☝👆".includes(em))
-        vote(ctx, 1)
-    else if (em === "👎")
-        vote(ctx, -1)
-    else */
     lastMessage[ctx.chat.id] = ctx.message
     lastMessagePosta[ctx.chat.id] = ctx.message
 })
@@ -41,74 +37,110 @@ bot.on(['audio', 'document', 'video', 'voice', 'contact', 'location', 'venue', '
 
 bot.on('text', (ctx) => {
 
-    //try {
+    try {
 
     const msgtext = toAscii(ctx.message.text)
     if (ctx.message.chat.id == -1001092578031)
         bot.telegram.forwardMessage(-255486826, ctx.message.chat.id, ctx.message.message_id) //Test
-    /*if ("☝👆".includes(Array.from(msgtext)[0])) {
-        vote(ctx, 1)
-    } else if ((Array.from(msgtext)[0]) === '👎') {
-        vote(ctx, 1)
-    } else*/ 
+
     if (msgtext === '+1 @tuvieja_bot') {
         voteFor(ctx, WITTY, "WittyBot", 1)
     } else if (msgtext === '-1 @tuvieja_bot') {
         voteFor(ctx, WITTY, "WittyBot", -1)
     } else if (msgtext.match("^(k?(j|a| )* )?\\+[1-9][0-9]*( .*)?$")) {
+        console.log(msgtext)
         vote(ctx, 1)
     } else if (msgtext.match("^(k?(j|a| )* )?\\-[1-9][0-9]*( .*)?$")) {
         vote(ctx, -1)
     } else if (msgtext.includes("/scoreboard")) {
-        show(ctx, "dict", false, "Scores:")
+        var dicts = dictsFromLog(ctx.chat.id) 
+        show(ctx, dicts[0], false, "Scores:")
     } else if (msgtext.includes("/voters")) {
-        show(ctx, "voters", true, "Voters:")
-    } else if (ctx.chat.id == ME) {
-        var orig = ctx.message.reply_to_message
-        var path = process.cwd();
-        if (msgtext.includes("/load_scores")) {
-            fs.writeFile(path + "/dict", orig.text, function(err) {}); 
-            ctx.reply("OK")
-        } else if (msgtext.includes("/load_voters")) {
-            fs.writeFile(path + "/voters", orig.text, function(err) {}); 
-            ctx.reply("OK")
-        } else if (msgtext.includes("/load_graph")) {
-            fs.writeFile(path + "/graph", orig.text, function(err) {}); 
-            ctx.reply("OK")
-        } else if (msgtext.includes("/full_stats")) {
-            var buffer = fs.readFileSync(path + "/dict");
-            ctx.reply(JSON.parse(buffer))
-          
-            var buffer = fs.readFileSync(path + "/voters");
-            ctx.reply(JSON.parse(buffer))
-            var buffer = fs.readFileSync(path + "/graph");
-            ctx.reply(JSON.parse(buffer))
-        }
+        var dicts = dictsFromLog(ctx.chat.id)
+        show(ctx, dicts[1], true, "Voters:")
     } else if (msgtext.includes("/full_stats")) {
-        show(ctx, "dict", true, "Scores:")
-        show(ctx, "voters", true, "Voters:")
-        showTopFromGraph(ctx)
+        if (ctx.chat.id == ME) {
+          var dicts = dictsFromLog() 
+          for (var i in dicts)
+            ctx.reply(dicts[i]) 
+        } else {
+          yearReport(ctx)
+        }
     } else if (msgtext.includes("conto?")) {
         if (ctx.message.reply_to_message)
           ctx.reply(conto(ctx.chat.id, ctx.message.reply_to_message) ? "Sí" : "No")
-        else if (lastMessage[ctx.chat.id])
+        else
           ctx.reply(conto(ctx.chat.id, lastMessagePosta[ctx.chat.id]) ? "Sí" : "No")
-    } else {
+    } else if (msgtext.includes("/year_report")) {
+        var year = msgtext.split("_")[2]
+        yearReport(ctx, year)
+    } else if (lastMessage[ctx.chat.id]) {
         lastMessage[ctx.chat.id] = ctx.message
     }
     lastMessagePosta[ctx.chat.id] = ctx.message
   
-    /*} catch (err) {
+    } catch (err) {
         bot.telegram.forwardMessage(-258588711, ctx.message.chat.id, ctx.message.message_id)
-    }*/
+    }
 })
 
-//bot.hears('+1', (ctx) => vote(ctx, 1))
-//bot.hears('👍', (ctx) => vote(ctx, 1))
-//bot.hears('☝', (ctx) => vote(ctx, 1))
-//bot.hears('👆', (ctx) => vote(ctx, 1))
-//bot.hears('👎', (ctx) => vote(ctx, -1))
-//bot.hears('-1', (ctx) => vote(ctx, -1))
+const yearReport = (ctx, year) => {
+    var dicts = dictsFromLog(ctx.chat.id, year)
+    show(ctx, dicts[0], true, "Scores:")
+    show(ctx, dicts[1], true, "Voters:")
+    showTopFromGraph(ctx, dicts[2])
+    showBestWorstComment(ctx, dicts[3])
+    pagerank(ctx, dicts[2])
+}
+
+const pagerank = (ctx, graph) => {
+    if (ctx.chat.id in graph) {
+        var chatGraph = graph[ctx.chat.id]
+        var matrix = []
+        var i = 0
+        var reverse = {}
+        for (var key in chatGraph) {
+          reverse[key] = i
+          i++
+        }
+        var forward = Object.keys(chatGraph)
+        //console.log(forward, reverse)
+
+        for (var key in chatGraph) {
+          var vector = []
+          for (var key_2 in chatGraph[key]) {
+            if (key_2 in reverse)
+              vector[reverse[key_2]] = chatGraph[key][key_2]
+          }
+          //Matrixes.softmax(vector, i)
+          Matrixes.normalize_min0(vector, i)
+          matrix[reverse[key]] = vector
+        }
+      
+        Matrixes.transpose(matrix)
+        var res = Matrixes.pagerank(matrix)
+        
+        var tuples = []
+        for (i = 0; i < forward.length; i++)
+            tuples.push([findName(forward[i]), res[i]])
+      
+        tuples.sort(function(a, b) {
+            a = a[1];
+            b = b[1];
+
+            return a < b ? 1 : (a > b ? -1 : 0);
+        })
+      
+        //console.log(res)
+
+        var answer = "Pangerank:\n"
+        for (var i = 0; i < tuples.length; i++) {
+            answer += tuples[i][0] + ": " + tuples[i][1].toFixed(4) + '\n'
+        }
+        ctx.reply(answer)
+        
+    }
+}
 
 const getFrom = function(dicc, key) {
     if (key in dicc)
@@ -117,70 +149,132 @@ const getFrom = function(dicc, key) {
         return {}
 }
 
-const updateDict = function(ctx, scorerId, name, plusValue, minusValue) {
-    var path = process.cwd();
-    var buffer = fs.readFileSync(path + "/dict");
-    var dict = JSON.parse(buffer)
+const updateDict = function(chatId, scorerId, value, dict) {
   //ctx.telegram.sendMessage(160565993, buffer.toString())
-  var chatScores = getFrom(dict, ctx.chat.id)
+  if (scorerId == undefined) return
+  
+  const plusValue = (value + 1)/2
+  const minusValue = 1 - plusValue
+  
+  var chatScores = getFrom(dict, chatId)
   if (scorerId in chatScores)
-      chatScores[scorerId] = {user: name, p: chatScores[scorerId].p + plusValue, m: chatScores[scorerId].m + minusValue}
+      chatScores[scorerId] = {p: chatScores[scorerId].p + plusValue, m: chatScores[scorerId].m + minusValue}
   else
-      chatScores[scorerId] = {user: name, p: plusValue, m: minusValue}
+      chatScores[scorerId] = {p: plusValue, m: minusValue}
 
-  dict[ctx.chat.id] = chatScores
-
-  fs.writeFile(path + "/dict", JSON.stringify(dict, null, 4), function(err) {}); 
-}
-
-const updateVoters = function(ctx, fromUser, plusValue, minusValue) {
-    var path = process.cwd();
-    var buffer = fs.readFileSync(path + "/voters");
-    var voters = JSON.parse(buffer)
-  //ctx.telegram.sendMessage(160565993, buffer.toString())
-  const chatVoters = getFrom(voters, ctx.chat.id)
-  if (fromUser.id in chatVoters)
-      chatVoters[fromUser.id] = {user: fromUser.first_name, p: chatVoters[fromUser.id].p + plusValue, m: chatVoters[fromUser.id].m + minusValue}
-  else
-      chatVoters[fromUser.id] = {user: fromUser.first_name, p: plusValue, m: minusValue}
-
-  voters[ctx.chat.id] = chatVoters
-
-  fs.writeFile(path + "/voters", JSON.stringify(voters, null, 4), function(err) {}); 
+  dict[chatId] = chatScores
 
 }
 
-const updateGraph = function(ctx, scorerId, fromUser, name, value) {
-    var path = process.cwd();
-    var buffer = fs.readFileSync(path + "/graph");
-    var graph = JSON.parse(buffer)
+const updateGraph = function(chatId, scorerId, fromId, value, graph) {
   //ctx.telegram.sendMessage(160565993, buffer.toString())
-  const chatGraph = getFrom(graph, ctx.chat.id)
-  const fromGraph = getFrom(chatGraph, fromUser.id)
+  const chatGraph = getFrom(graph, chatId)
+  const fromGraph = getFrom(chatGraph, fromId)
   if (scorerId in fromGraph)
       fromGraph[scorerId] += value
   else
       fromGraph[scorerId] = value
-  chatGraph[fromUser.id] = fromGraph
-  graph[ctx.chat.id] = chatGraph
-
-  fs.writeFile(path + "/graph", JSON.stringify(graph, null, 4), function(err) {}); 
+  chatGraph[fromId] = fromGraph
+  graph[chatId] = chatGraph
 
 }
 
-const voteFor = function(ctx, scorerId, name, value) {
+const dictsFromLog = function(chatid, year) {
+  var scorers = {}
+  var voters = {}
+  var graph = {}
+  var msgScores = {}
+  
+  if (year == undefined) 
+    year = new Date().getFullYear()
+  
+  var lines = fs.readFileSync('./log', 'utf-8').split(/\r?\n/)
+  
+  for (var i in lines) {
+    try {
+      var voto = JSON.parse(lines[i])
+      //console.log(year, voto.year)
+      if ((voto.year == year) && (voto.chatid == chatid || chatid == undefined)) {
+        loadVote(voto, scorers, voters, graph, msgScores)
+        //console.log(voto.value)
+      }
+    } catch(e) {
+      
+    }
+  }
+  
+  return [scorers, voters, graph, msgScores]
+}
+
+const showBestWorstComment = function(ctx, dict) {
+  if (ctx.chat.id in dict) {
+    
+    var chatDict = dict[ctx.chat.id]
+  
+    var maxKey = undefined
+    var minKey = undefined
+    var max = 0
+    var min = 0
+    for (var key in chatDict) {
+      if (chatDict[key].p > max) {
+        maxKey = key
+        max = chatDict[key].p
+      } else if (-chatDict[key].m < min) {
+        minKey = key
+        min = -chatDict[key].m
+      }
+    }
+
+    if (max > 0) {
+      var best = "Best comment (" + chatDict[maxKey].p + " upvotes)"
+      bot.telegram.sendMessage(ctx.chat.id, best, {reply_to_message_id: maxKey})
+    }
+
+    if (min < 0) {
+      var worst = "Worst comment (" + chatDict[minKey].m + " downvotes)"
+      bot.telegram.sendMessage(ctx.chat.id, worst, {reply_to_message_id: minKey})
+    }
+  }
+}
+
+const loadVote = function(voto, scorers, voters, graph, msgScores) {
+  var chatId = voto.chatid
+  var fromId = voto.from
+  var scorerId = voto.to
+  var value = voto.value
+
+  updateDict(chatId, scorerId, value, scorers)
+  updateDict(chatId, fromId, value, voters)
+  updateGraph(chatId, scorerId, fromId, value, graph)
+  updateDict(chatId, voto.msgidTo, value, msgScores)
+  
+}
+
+const voteFor = function(ctx, scorerId, name, value, toId) {
   const plusValue = (value + 1)/2
   const minusValue = 1 - plusValue
 
   const fromUser = ctx.message.from
 
-  updateDict(ctx, scorerId, name, plusValue, minusValue)
-  updateVoters(ctx, fromUser, plusValue, minusValue)
-  updateGraph(ctx, scorerId, fromUser, name, value)
-
-  var log = fromUser.first_name + " ==" + value + "=> " + name
-  fs.appendFile("./log", "chatid: " + ctx.chat.id + " msgid: " + ctx.message.message_id + " - " + log + "\n", function(err) {}); 
-  ctx.telegram.sendMessage(160565993, ctx.chat.id + ": " + log)
+  var voto = {
+    year: new Date().getFullYear(),
+    chatid: ctx.chat.id,
+    msgid: ctx.message.message_id,
+    from: fromUser.id,
+    to: scorerId,
+    msgidTo: toId,
+    value: value
+  }
+  
+  fs.appendFile("./log", JSON.stringify(voto) + "\n", function(err) {}); 
+  ctx.telegram.sendMessage(160565993, ctx.chat.id + ": " + fromUser.first_name + " ==" + value + "=> " + name)
+  
+  
+  var buffer = fs.readFileSync("./names");
+  var names = JSON.parse(buffer)
+  names[fromUser.id] = fromUser.first_name 
+  names[scorerId] = name
+  fs.writeFile("./names", JSON.stringify(names, null, 4), function(err) {}); 
 
 }
 
@@ -190,7 +284,7 @@ const vote = function(ctx, value) {
   if (!orig && ctx.chat.id in lastMessage)
     orig = lastMessage[ctx.chat.id]
   if (!orig)
-    return 1
+    return
   
   //ctx.reply(orig)
 
@@ -198,39 +292,27 @@ const vote = function(ctx, value) {
     //ctx.reply("Self upvote")
     return
    
-  if (orig.message_id in messageLikers)
-    var likers = messageLikers[orig.message_id]
-  else
-    var likers = []
-
-  if (likers.includes(ctx.message.from.id))
-    //ctx.reply("Already upvoted")
+  if (yaLoVoto(ctx.chat.id, orig, ctx.message.from.id, value)) {
     return
+  }
   
   if (conto(ctx.chat.id, orig)) {
     return
   }
 
-  likers.push(ctx.message.from.id)
-  messageLikers[orig.message_id] = likers
-  //ctx.reply(messageLikers)
   var scorer = orig.from
 
-  voteFor(ctx, scorer.id, scorer.first_name, value)
+  voteFor(ctx, scorer.id, scorer.first_name, value, orig.message_id)
 }
 
-var show = function(ctx, diccName, full, name) {
-    var path = process.cwd();
-    var buffer = fs.readFileSync(path + "/" + diccName);
-    var dicc = JSON.parse(buffer)
+var show = function(ctx, dicc, full, name) {
+    var answer = name + "\n"
     if (ctx.chat.id in dicc) {
-        var chatScores = dicc[ctx.chat.id]
-
-        var answer = name + "\n"
+        var chatScores = dicc[ctx.chat.id]       
         var tuples = [];
 
         for (var key in chatScores)
-            tuples.push([chatScores[key].user, chatScores[key].p, chatScores[key].m]);
+            tuples.push([findName(key), chatScores[key].p, chatScores[key].m]);
 
         tuples.sort(function(a, b) {
             if (full) {
@@ -253,30 +335,20 @@ var show = function(ctx, diccName, full, name) {
             answer += '\n'
         }
         ctx.reply(answer)
+    } else {
+        ctx.reply(answer + "No records")
     }
 }
 
-var findName = function(chatId, id) {
-    var path = process.cwd();
-    var buffer = fs.readFileSync(path + "/voters");
-    var voters = JSON.parse(buffer)
-    //var path = process.cwd();
-    var buffer = fs.readFileSync(path + "/dict");
-    var dict = JSON.parse(buffer)
-    var chatVoters = getFrom(voters, chatId)
-    var chatScores = getFrom(dict, chatId)
-    if (id in chatVoters)
-        return chatVoters[id].user
-    else if (id in chatScores)
-        return chatScores[id].user
+var findName = function(id) {
+    var buffer = fs.readFileSync("./names");
+    var names = JSON.parse(buffer)
+    if (id in names)
+        return names[id]
     return "Unknown"
 }
 
-var showTopFromGraph = function(ctx) {
-    var path = process.cwd();
-    var buffer = fs.readFileSync(path + "/graph");
-    var graph = JSON.parse(buffer)
-
+var showTopFromGraph = function(ctx, graph) {
     var tuples = [];
     if (ctx.chat.id in graph) {
 
@@ -284,7 +356,7 @@ var showTopFromGraph = function(ctx) {
 
         for (var from in chatGraph) {
             for (var to in chatGraph[from]) {
-                tuples.push([findName(ctx.chat.id, from), findName(ctx.chat.id, to), chatGraph[from][to]])
+                tuples.push([findName(from), findName(to), chatGraph[from][to]])
             }
         }
 
@@ -308,22 +380,69 @@ var showTopFromGraph = function(ctx) {
     }
 }
 
-var conto = (chatid, msg) => {
-  var msgid = msg.message_id
-  console.log(msgid)
+const yaLoVoto = (chatid, msg, fromId, value) => {
+  
   var lines = fs.readFileSync('./log', 'utf-8').split(/\r?\n/)
   
   for (var i in lines) {
-    var line = lines[i]
-    
-    if (line.split(" ")[1] == chatid && line.split(" ")[3] == msgid)
-      return true
+    try {
+      var voto = JSON.parse(lines[i])
+      if (voto.chatid == chatid 
+          && voto.msgidTo == msg.message_id
+          && voto.from == fromId
+          && voto.value == value)
+        return true
+    } catch(e) {
+    }
   }
+  
   return false
 }
 
-/*bot.command('scoreboard', (ctx) => showScoreboard(ctx))
-bot.command('scoreboard@thumbsup_score_bot', (ctx) => showScoreboard(ctx))*/
+const conto = (chatid, msg) => {
+  
+  var msgid = msg.message_id
+  var lines = fs.readFileSync('./log', 'utf-8').split(/\r?\n/)
+  
+  for (var i in lines) {
+    try {
+      var voto = JSON.parse(lines[i])
+      if (voto.chatid == chatid && voto.msgid == msgid)
+        return true
+    } catch(e) {
+      
+    }
+  }
+  
+  return false
+}
+
+const getAllGroups = () => {
+    
+  var lines = fs.readFileSync('./log', 'utf-8').split(/\r?\n/)
+  
+  var res = new Set([])
+  
+  for (var i in lines) {
+    try {
+      var voto = JSON.parse(lines[i])
+      res.add(voto.chatid)
+    } catch(e) {
+      
+    }
+  }
+  
+  return Array.from(res)
+}
+schedule.scheduleJob('30 4 1 1 *', () => {
+  var groups = getAllGroups()
+  for (var i in groups) {
+    var group = groups[i]
+    bot.telegram.sendMessage(group, "Happy new year!")
+    bot.telegram.sendMessage(group, "Scores will be reset now")
+    bot.telegram.sendMessage(group, "You can see them at any time using the command /year_report_" + (new Date().getFullYear() - 1))
+  }
+})
 
 bot.startPolling()
 
